3db5c664c0ad28dcdb58c3c54e737cfd3d04f9ee
Thulisile <thulisilebuzani@gmail.com>
2023-06-22 03:38:05 -0700

    Multiple mains

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
+ * main - Entry point of the program
+ * Return: Always 0
+ */
 int main(void)
 {
-int i;
-printf("Infinite loop incoming :(\n");
-i = 0;
-/*while (i < 10)*/
-/*{*/
-/*putchar(i);*/
-/*}*/
-printf("Infinite loop avoided! \\o/\n");
-return (0);
+    int i;
+
+    printf("Infinite loop incoming :(\n");
+    i = 0;
+    while (i < 10)
+    {
+        putchar(i + '0');  // Print the value of i as a character
+        i++;
+    }
+    printf("\nInfinite loop avoided! \\o/\n");
+    return (0);
 }
